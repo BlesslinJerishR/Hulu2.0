4f12873e7b6a430a2135ebc3d0b823c5e1606037
Mastermind <mastermindx33@gmail.com>
2023-03-21 12:25:09 +0530

    __init__ Hulu

--- hulu/src/pages/index.js
+++ hulu/src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello</h1>
+      <h1>Hulu</h1>
     </>
   );
 }
